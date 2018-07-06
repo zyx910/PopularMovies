50b0ac8aa499e836eab5a6afe8b26e1090d8973b
zyx910 <zyx_910@163.com>
2018-07-06 14:36:45 +0800

    Fix memory leaks issue

--- app/src/main/java/com/example/android/popularmovies/ReviewActivity.java
+++ app/src/main/java/com/example/android/popularmovies/ReviewActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
 
 import com.example.android.popularmovies.utilities.MovieJSONObject;
 import com.example.android.popularmovies.utilities.NetWorkUtils;
